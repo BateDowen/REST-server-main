1fde6ecce3a0178b3ed9164935966d363ad45ee7
BateDowen <miroslav.dimitrov1994@gmail.com>
2023-06-26 09:38:51 +0300

    add auth-middleware in all routes

--- routes/feed.js
+++ routes/feed.js
@@ -7,16 +7,16 @@ const isAuth = require('../middlewares/is-auth.js');
 
 router.get('/posts',isAuth,feedController.getPosts);
 
-router.post('/post',[
+router.post('/post',isAuth,[
     body('title').trim().isLength({min:5}),
     body('content').trim().isLength({min:5})
 ], feedController.createPosts);
 
-router.get('/post/:postId',feedController.getPost);
+router.get('/post/:postId',isAuth,feedController.getPost);
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId',isAuth, feedController.deletePost);
 
-router.put('/post/:postId',[
+router.put('/post/:postId',isAuth,[
     body('title').trim().isLength({min:5}),
     body('content').trim().isLength({min:5})
 ], feedController.updatePost);
